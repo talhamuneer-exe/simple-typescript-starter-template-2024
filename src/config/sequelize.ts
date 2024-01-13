5eb2b368efd7fa4ee64cf993e87b8387486dba86
Talha Muneer <talhamuneer.exe@gmail.com>
2024-01-13 21:07:48 +0500

    feat(2024): Fix some minor issues

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,4 +1,5 @@
-import { Sequelize } from 'sequelize';
-export default new Sequelize(
-  `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-);
+// Run "npm i sequelize" before uncommenting it.
+// import { Sequelize } from 'sequelize';
+// export default new Sequelize(
+//   `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+// );
--- src/middleware/jwt-verification.ts
+++ src/middleware/jwt-verification.ts
@@ -1,25 +1,27 @@
-import { NextFunction, Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+//Run "npm i jsonwebtoken" Before uncommenting this.
 
-function verifyToken(req: Request, res: Response, next: NextFunction) {
-  let token = req.headers['authorization'];
+// import { NextFunction, Request, Response } from 'express';
+// import jwt from 'jsonwebtoken';
 
-  if (!token) {
-    return res.status(401).json({ message: 'Unauthorized' });
-  }
-  // Check if the header starts with "Bearer "
-  if (token.startsWith('Bearer ')) {
-    // Remove "Bearer " prefix
-    token = token.slice(7);
-  }
-  jwt.verify(token, process.env.SECRET_KEY!, (err: any, decoded: any) => {
-    if (err) {
-      return res.status(401).json({ message: 'Invalid token' });
-    }
-    req.headers['empId'] = decoded.employeeId;
-    // Continue to the next middleware or route handler
-    next();
-  });
-}
+// function verifyToken(req: Request, res: Response, next: NextFunction) {
+//   let token = req.headers['authorization'];
 
-export default verifyToken;
+//   if (!token) {
+//     return res.status(401).json({ message: 'Unauthorized' });
+//   }
+//   // Check if the header starts with "Bearer "
+//   if (token.startsWith('Bearer ')) {
+//     // Remove "Bearer " prefix
+//     token = token.slice(7);
+//   }
+//   jwt.verify(token, process.env.SECRET_KEY!, (err: any, decoded: any) => {
+//     if (err) {
+//       return res.status(401).json({ message: 'Invalid token' });
+//     }
+//     req.headers['empId'] = decoded.employeeId;
+//     // Continue to the next middleware or route handler
+//     next();
+//   });
+// }
+
+// export default verifyToken;
